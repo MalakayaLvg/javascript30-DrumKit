6e8d887ccae017624e886a38c5705522fc6cf754
Oslynx <malakayalvg@gmail.com>
2023-11-03 17:57:26 +0100

    revisité en Playing Piano, Version finale

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 window.addEventListener('keydown',function (event) {
-    console.log(event.code)
+    //console.log(event.code)
     const audio = document.querySelector(`audio[class="${event.code}"]`)
-    console.log(audio)
+    //console.log(audio)
     const key = document.querySelector(`.touch${event.code}`)
-    console.log(key)
+    //console.log(key)
     key.classList.add("playing")
     if (!audio) return
     audio.currentTime = 0;
     audio.play()
+});
+
+keys = document.querySelectorAll('.key')
+console.log(keys)
+
+keys.forEach((key)=> {
+    console.log(key)
+    key.addEventListener('transitionend', ()=>{
+        key.classList.remove("playing")
+    });
 })
+
+
+
